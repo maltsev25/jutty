e4e26a61c37d0eb0906201ea7fa899ae02196d44
Gleb <49669778+maltsev25@users.noreply.github.com>
2020-04-15 11:11:23 +1000

    Update ConfigLog4j.ts

--- src/ConfigLog4j.ts
+++ src/ConfigLog4j.ts
@@ -4,8 +4,8 @@ import { LoggerFactoryOptions, LFService, LogGroupRule, LogLevel } from "typescr
 // * One for any logger with a name starting with model, to log on debug
 // * The second one for anything else to log on info
 const options = new LoggerFactoryOptions()
-    .addLogGroupRule(new LogGroupRule(new RegExp("model.+"), LogLevel.Debug))
-    .addLogGroupRule(new LogGroupRule(new RegExp(".+"), LogLevel.Debug));
+    .addLogGroupRule(new LogGroupRule(new RegExp("model.+"), LogLevel.Info))
+    .addLogGroupRule(new LogGroupRule(new RegExp(".+"), LogLevel.Info));
 
 // Create a named loggerfactory and pass in the options and export the factory.
 // Named is since version 0.2.+ (it's recommended for future usage)
